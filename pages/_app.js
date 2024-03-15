d2196ed4e44ff90ea1def3b7fc99f9766e136025
Dimas Adimo <dimasadi1308@gmail.com>
2024-03-15 15:25:32 +0700

    Initialized Project

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,16 @@
 import "@/styles/globals.css";
+import PropTypes from 'prop-types';
+import { ChakraProvider } from '@chakra-ui/react';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
+
+App.propTypes = {
+  Component: PropTypes.any,
+  pageProps: PropTypes.object,
+};
